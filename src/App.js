bee5878f37f11f010c2ee6b49efc15f0e408e3b5
David Huveau <d.huveau@gmail.com>
2018-10-30 17:07:57 +0100

    ajout notion Step pour piloter l'enchainement des composants

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
-
-import './App.css';
-import questions from './constants/questions';
-import players from './constants/players';
 import Questions from './components/Questions';
 import DeadJackpot from './components/DeadJackpot';
-
-
 import Home from './components/Home';
+import Finish from './components/Finish';
+import './App.css';
+
+import jsonQuestions from './constants/questions';
+import jsonPlayers from './constants/players';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <DeadJackpot listPlayers ={players} />
-      <Questions listPlayers ={players} question ={questions[0]} />
-        {/* <Home  players ={players} /> */}
-      </div>
-    );
+
+  state = {
+    step: -1
   }
+
+  componentDidMount() {
+    this.setState({
+      step: 4
+    })
+  }
+
+  render() {
+    if (this.state.step === 2) {
+      return <Questions listPlayers ={jsonPlayers} question ={jsonQuestions[0]} />
+    }
+    else if (this.state.step === 3) {
+        return <DeadJackpot listPlayers ={jsonPlayers} />
+    }
+    else if (this.state.step === 1) {
+      return <Home listPlayers ={jsonPlayers} />
+    }
+    else if (this.state.step === 4) {
+      return <Finish listPlayers ={jsonPlayers[0]} />
+    }
+    else {
+      return <div>Error Step</div>
+    }
+  }
+
 }
 
 export default App;
--- src/components/DeadJackpot.js
+++ src/components/DeadJackpot.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class DeadJackpot extends Component {
     state = {  }
     render() { 
-        return (  );
+        return <div>DeadJackpot</div>;
     }
 }
  
