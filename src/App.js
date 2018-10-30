600c3a38117bd204e869620a6b53602c0ae0c8f3
samirkhennache <samirkhennache@gmail.com>
2018-10-30 16:27:01 +0100

    creation de DeadJackpot

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import questions from './constants/questions';
 import players from './constants/players';
 import Questions from './components/Questions';
+import DeadJackpot from './components/DeadJackpot';
 
 
 import Home from './components/Home';
@@ -12,7 +13,8 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Questions listPlayers ={players} questions ={questions[0]} />
+        <DeadJackpot listPlayers ={players} />
+      <Questions listPlayers ={players} question ={questions[0]} />
         {/* <Home  players ={players} /> */}
       </div>
     );
