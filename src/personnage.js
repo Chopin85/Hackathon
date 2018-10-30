f593a95d86ea82823158b05487458363e913aebc
Prudence Gaboriau <prudence.gaboriau@gmail.com>
2018-10-30 12:44:33 +0100

    composant personnage

---
+++ src/personnage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Personnage = ({avatar, image, handleChange}) => (
+    <div>
+    <div>{avatar}</div>
+    <img src={image}alt={avatar}/>
+    <form>
+        <input type="text" onChange={handleChange} />
+    </form>
+    </div>
+)
+export default Personnage;
