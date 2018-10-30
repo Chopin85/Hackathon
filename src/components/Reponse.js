5b7a55bca34271e5f5844104e506698460d41f49
Chopin85 <p.cata85@gmail.com>
2018-10-30 21:47:14 +0100

    Update checkbox not finish

--- src/components/Reponse.js
+++ src/components/Reponse.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, {Component} from "react";
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
 
-
-const Reponse = props => {
-
-    const verifyReponse = (resp, player) => {
-        (resp === props.question.correctAnswer) ?  props.response(player , true) : props.response(player, false)
+class Reponse extends Component {
+     verifyReponse = (resp, player) => {
+        (resp === this.props.question.correctAnswer) ?  this.props.response(player , true) : this.props.response(player, false)
     }
 
-    console.log(props)
-  return (
-    <div>
-      <p>{props.question.question}</p>
-      {props.question.ansewers.map((x, index) =>
-      <button key={index} onClick={() => verifyReponse(index, 1)}>
-        {x}
-      </button>
-      )}
-    </div>
-  );
-};
+        state = {
+          checkedA: false,
+        };
+      
+        handleChange = name => event => {
+          this.setState({ [name]: event.target.checked });
+        };
+      
+
+        
+        
+    render() {
+        console.log(this.state.checkedA)
+        return ( 
+            <div>
+            <p>{this.props.question.question}</p>
+            {this.props.question.ansewers.map((x, index) =>
+            <button key={index} onClick={() => this.verifyReponse(index, 1)}>
+              {x}
+            </button>
+            )}
+            <Checkbox
+                checked={this.state.checkedA}
+                onChange={this.handleChange('checkedA')}
+                value="checkedA"
+              />firklcrehc
+              <br/>
+            <Button variant="contained">
+                ---Valid---
+            </Button>
+          </div>
+          );
+    }
+}
+
+
 export default Reponse;
+
