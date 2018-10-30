0b75febff2b0ce1ff70ecbdfc519609e2291c6bc
Prudence Gaboriau <prudence.gaboriau@gmail.com>
2018-10-30 16:31:51 +0100

    composant dead jackpot

---
+++ src/components/DeadJackpot.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const 
+class DeadJackpot extends Component {
+    state = {  }
+
+    render() { 
+        return ( 
+        <div>
+
+        </div> );
+    }
+}
+ 
+export default DeadJackpot;
