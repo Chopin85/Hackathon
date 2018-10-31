8c782d06d4b81a9bc8b6f831e94ec4061c938f5d
David Huveau <d.huveau@gmail.com>
2018-10-31 11:27:06 +0100

    rendre cliquable les labels des button radio des réponses

--- src/components/Reponse.js
+++ src/components/Reponse.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
-import Radio from '@material-ui/core/Radio';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper';
-
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import './question.css'
+
 
 
 class Reponse extends Component {
@@ -28,27 +28,34 @@ class Reponse extends Component {
             <div style={{background: '#212121'}}>
             <p style={{color: "White", display: 'flex', justifyContent: 'center', fontSize:'20px'}} >{this.props.question.question}</p>
             <div className="response">
-            {/* {this.props.question.ansewers.map((x, index) =>
-            <button key={index} onClick={() => this.verifyReponse(index, 1)}>
-              {x}
-            </button>
-            )} */}
-
+            <RadioGroup
+                // className="radio"
+                // style={{color: "White"}}
+                aria-label="Gender"
+                name="gender1"
+                value={this.state.selectedValue}
+                onChange={this.handleChange}
+                >
                 {this.props.question.ansewers.map((x, index) =>
-                        <div className="radio">
+                        // <div className="radio">
                       
                       
-                        <Radio className="check"
-                            checked={this.state.selectedValue === index.toString()}
-                            onChange={this.handleChange}
-                            value={index.toString()}
-                            name="radio-button-demo"
-                            aria-label="A"
-                        />
-                        {x}
+                        // <Radio className="check"
+                        //     checked={this.state.selectedValue === index.toString()}
+                        //     onChange={this.handleChange}
+                        //     value={index.toString()}
+                        //     name="radio-button-demo"
+                        //     aria-label="A"
+                        // />
+                        // {x}
                         
-                        </div>
+                        // </div>
+                        <FormControlLabel value={index.toString()} control={<Radio color="secondary"/>} label={x}/>
                 )}
+                {/* <FormControlLabel value="female" control={<Radio />} label="Female" />
+                <FormControlLabel value="male" control={<Radio />} label="Male" /> */}
+
+                </RadioGroup>
               <br/>
             </div>
               <div className="button-response" style={{display: 'flex', justifyContent: 'center'}}>
