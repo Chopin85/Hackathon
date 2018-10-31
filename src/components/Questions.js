c128c520c7dda6e5e95c774e9d5b585a43617d52
Prudence Gaboriau <prudence.gaboriau@gmail.com>
2018-10-31 01:22:33 +0100

    ajout textes page index et question

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -8,6 +8,9 @@ class Questions extends Component {
         return ( 
             <div>
                 <Header />
+                <div className="bloc-presentation">
+                    <p className="text-presentation">Lors de la cérémonie, tout ne s’est pas passé comme prévu, vous avez invoqué « .. » et vous n’auriez pas du.. Une seule chance pour se sortir vivant de ce mauvais pas, répondre correctement aux questions de l’esprit maléfique. Une mauvaise réponse et c’est la mort assurée d’un membre de votre équipe.</p>
+                </div>
                 <Reponse response={this.props.response} question={this.props.question}/>
             </div>
           );
