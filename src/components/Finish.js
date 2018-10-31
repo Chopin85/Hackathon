da4c1bd1e7003398ddc483ebd3f50570e044f42a
David Huveau <d.huveau@gmail.com>
2018-10-31 07:22:12 +0100

    mise en forme de Finish

--- src/components/Finish.js
+++ src/components/Finish.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Avatars from './Avatar'
 
 // import './finisn.css'
-
+const titleStyle = {
+    fontWeight: '100',
+    fontSize: 'calc(12px + 3vw)',
+    color: 'white',
+    // position: 'relative',
+    marginBottom: '15vh'
+};
 
 class Finish extends React.Component{
 
@@ -12,14 +18,15 @@ class Finish extends React.Component{
     render() {
         console.log('finish', this.state.CurrentPlayers)
         return (
-            <div>
-                <h1>Finish</h1>
-                {this.state.CurrentPlayers.map((element,index) => {
-                return(
-                    <Avatars dead={element.isAlive[1]} key={index} image={'/img/' + element.id + '.png'} avatar={element.avatar}  player= {element.player} />
-                    )
-                }
-            )}
+            <div style={{textAlign:"center"}}>
+                <h1 style={titleStyle}>Finish</h1>
+                <div className="Avatar_finish">
+                    {this.state.CurrentPlayers.map((element,index) => {
+                        return(
+                            <Avatars isAlive={true} key={index} image={'/img/' + element.id + '.png'} avatar={element.avatar}  player= {element.player} />
+                        )}
+                    )}
+                </div>
         </div>
         )
     }
